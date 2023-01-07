7cfff64a189ca3f48c4746eca692649bdf5c8c19
Nicolas Hervy <nicolas@hervy.se>
2023-01-07 17:38:36 +0100

    react added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-import netlify from '@astrojs/netlify/functions'
+import netlify from '@astrojs/netlify/functions';
+
+// https://astro.build/config
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), react()],
   output: 'server',
-  adapter: netlify(),
-})
+  adapter: netlify()
+});
