bda4174d5b016b8edc50c98e89c5a0173d36dabc
Nicolas Hervy <nicolas@hervy.se>
2023-01-24 22:54:54 +0100

    Updated to astro v2.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,13 @@ import sitemap from '@astrojs/sitemap';
 import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
-import react from "@astrojs/react";
+import react from '@astrojs/react';
+
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://hervy.netlify.com/',
   integrations: [mdx(), sitemap(), react()],
   output: 'server',
   adapter: netlify()
