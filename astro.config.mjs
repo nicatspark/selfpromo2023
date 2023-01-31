7c3f3732225c055bcbf72a2317a6ec0940eac119
Nicolas Hervy <nicolas@hervy.se>
2023-01-31 10:44:21 +0100

    Added image tools. Converted image to img.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,17 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
-import netlify from '@astrojs/netlify/functions';
+import netlify from '@astrojs/netlify/functions'
 
 // https://astro.build/config
-import react from '@astrojs/react';
-
-// https://astro.build/config
-
-// https://astro.build/config
-import image from "@astrojs/image";
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://hervy.netlify.com/',
-  integrations: [mdx(), sitemap(), react(), image()],
+  integrations: [mdx(), sitemap(), react()],
   output: 'server',
-  adapter: netlify()
-});
+  adapter: netlify(),
+})
